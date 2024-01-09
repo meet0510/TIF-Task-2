673974a7a5be606fe48e1705d2f69184f1b5592a
meet <meetunpatel.0510@gmail.com>
2024-01-09 09:25:44 +0530

    Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import HomeLayout from "@containers/home/HomeLayout";
-import Layout from "../../components/Layout";
-import DataProvider from "../../containers/home/DataProvider";
+import Layout from "../components/Layout";
+import DataProvider from "../containers/home/DataProvider";
 
 const Home: NextPage = () => {
   return (
